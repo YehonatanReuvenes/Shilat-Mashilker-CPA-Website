b2f22405d41d66810734f201ba113283d383d7f8
JonatanRuvenes <116504321+JonatanRuvenes@users.noreply.github.com>
2023-07-18 22:53:22 +0300

    adding head, foot

---
+++ load_Head_Foot.js
@@ -0,0 +1,15 @@
+// Load header content
+var headerContainer = document.getElementById('header-container');
+fetch('head.html')
+  .then(response => response.text())
+  .then(content => {
+    headerContainer.innerHTML = content;
+  });
+
+// Load footer content
+var footerContainer = document.getElementById('footer-container');
+fetch('foot.html')
+  .then(response => response.text())
+  .then(content => {
+    footerContainer.innerHTML = content;
+  });
