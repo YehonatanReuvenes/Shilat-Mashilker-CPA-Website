8b47ded9d14332faefbedd3e9180df06b129252d
JonatanRuvenes <116504321+JonatanRuvenes@users.noreply.github.com>
2023-07-30 01:01:28 +0300

    adding lines on and under the nav text

--- load_Head_Foot.js
+++ load_Head_Foot.js
@@ -4,6 +4,27 @@ fetch('head.html')
   .then(response => response.text())
   .then(content => {
     headerContainer.innerHTML = content;
+
+    var page = headerContainer.getAttribute('data-page');
+    if (page) {
+      switch(page){
+        case 'Home':
+            aLink = document.getElementById('home-link').style.textDecoration = "underline overline"
+          break;
+        case 'Services':
+            aLink = document.getElementById('services-link').style.textDecoration = "underline overline"
+          break;
+        case 'About':
+            document.getElementById('about-link').style.textDecoration = "underline overline"
+          break;
+        case 'Articles':
+            document.getElementById('articles-link').style.textDecoration = "underline overline"
+          break;
+        case 'MakeContact':
+            aLink = document.getElementById('make-contact-link').style.textDecoration = "underline overline"
+          break;
+      }
+    }
   });
 
 // Load footer content
